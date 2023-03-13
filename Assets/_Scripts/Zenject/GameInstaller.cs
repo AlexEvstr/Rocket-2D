3708ad0bb9a3af7c3ed70d1c53eeb677747ff1da
Alexander Evstratov <alexdrugev@me.com>
2023-03-13 17:42:15 +0300

    Add zenject.

---
+++ Assets/_Scripts/Zenject/GameInstaller.cs
@@ -0,0 +1,24 @@
+using Evstr.Generals;
+using Evstr.States;
+using System;
+using Zenject;
+
+namespace Evstr.Zenject
+{
+    public class GameInstaller : MonoInstaller, IUpdateLoop
+    {
+        public event Action OnUpdate;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance<IUpdateLoop>(this);
+            Container.Bind<IInputSystem>().To<InputSystemAndroid>().AsSingle();
+
+            Container.Bind<StateMachine>().AsSingle();
+        }
+        public void Update()
+        {
+            OnUpdate?.Invoke();
+        }
+    }
+}
